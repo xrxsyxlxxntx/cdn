e76d790c6d548f266964d97bc9ab97f3c3c10335
xrxsyxlxxntx <39103740+xrxsyxlxxntx@users.noreply.github.com>
2018-06-01 03:21:42 +0800

    Create disqus.js

---
+++ blogger/disqus.js
@@ -0,0 +1,2 @@
+var disqus_shortname = &quot;melekteknologi&quot;;
+!function(){var e=document.createElement(&quot;script&quot;);e.type=&quot;text/javascript&quot;,e.async=!0,e.src=&quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;,(document.getElementsByTagName(&quot;head&quot;)[0]||document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(e)}();
